4e1680642214823a97f08b8c1a3422f2c06b8297
SallesCosta <newcapital.in@gmail.com>
2021-10-13 19:49:24 +0200

    dependencias

--- ReactWebPack/src/index.js
+++ ReactWebPack/src/index.js
@@ -1,11 +1,11 @@
 //aqui é o entryPointe que RECEBE/IMPORTA as funções dos outros arquivos
+//import { render } from 'react-dom' //shortHand not ation
+//var Title = require('./app')  //padrão CommonJS
 'use strict'
 
 import React from 'react'    //padrão EcmaScript2015
 import ReactDOM from 'react-dom' //padrão EcmaScript2015
-//import { render } from 'react-dom' //shortHand notation
 import Title from './app.js' //padrão EcmaScript2015
-//var Title = require('./app')  //padrão CommonJS
 
 
 ReactDOM.render(          //se fosse usar shortHand notation... essa linha ficaria apenas '  render(   '
---
+++ anotações.sh
@@ -0,0 +1,48 @@
+#helloWorld
+criei a pasta helloWorld
+
+criei um arquivo index.html
+
+instalei:
+npm install -g http-server
+
+ai eu dei um http-server e ja está rodando um servidor na porta 8080
+faz o mesmo que o liveserver que ja estou acostumado
+
+
+coloquei essa tag no body.. é a versao do react.. o conselho do professor é usar o ... umd/react.development...
+
+    scripts da aula.. não são os mais atuais, mas funcionam para o aprendizado
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.min.js"></script>
+
+--------------------------------------------------------------------------------
+#dependencias em dia - Daciuk
+#   https://github.com/SallesCosta/curso-reactjs-ninja/blob/master/versions-of-dependencies.md
+
+
+#ReactWebPack
+
+no terminal, para o package.json ser criado.. tem apenas um par de chaves dentro do arquivo
+echo {} >> package.json
+
+#instalar npm install --save-dev webpack@1  
+#npm install -g webpack@1
+#executar webpack
+#instalar npm install --save-dev webpack-dev-server@1
+#instalar npm install -g webpack-dev-server@1
+#executar para rodar um servidor webpack-dev-server
+
+--------------------------------------------------------------------------------
+
+#Modularizar a app
+
+--------------------------------------------------------------------------------
+
+# npm install --save react@15.4 react-dom@15.4
+
+#instalar npm install --save-dev babel-core@6 babel-loader@6 babel-preset-es2015@6 babel-preset-stage-0@6
+#instalar npm install --save-dev babel-preset-react@6
+
+
