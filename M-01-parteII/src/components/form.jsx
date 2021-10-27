74d714441deed19d6b99b912dab369ef085665c8
SallesCosta <newcapital.in@gmail.com>
2021-10-27 21:50:55 +0200

    aula41

--- M-01-parteII/src/components/form.jsx
+++ M-01-parteII/src/components/form.jsx
@@ -18,13 +18,18 @@ export class Formulario extends Component {
             })
           }} />
           <label>
-            <input
+            <input //controled
               type='checkbox'
               checked={this.state.checked}
               value='my-checkbox'
               onChange={(e) => this.setState({ checked: !this.state.checked})}
             />
-            Checkbox
+              Checkbox controlado
+            <input //controled
+              type='checkbox'
+              defaultChecked={false}
+            />
+            Checkbox Não controlado
           </label>
         </form>
       </div>
