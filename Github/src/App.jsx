570b900649463fd0712897fbdd05ebad446c5585
SallesCosta <newcapital.in@gmail.com>
2021-10-31 21:37:17 +0100

    marcação app GitHub

--- Github/src/App.jsx
+++ Github/src/App.jsx
@@ -1,43 +1,24 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='app'>
+      <div className='userInfo'>
+        <img src='https://avatars.githubusercontent.com/u/81476236?v=4' />
+        <h2>
+          <a target='_blank' href='https://github.com/SallesCosta'>Eduardo</a>
+        </h2>
+        <ul className='reposInfo'>
+          <li>- Repositórios: 100</li>
+          <li>- Seguidores: 10</li>
+          <li>- Seguindo: 10</li>
+        </ul>
+
+        <div className='acitons'>
+          <button>Ver repositórios</button>
+          <button>Ver Favoritos </button>
+        </div>
+      </div>
     </div>
   )
 }
