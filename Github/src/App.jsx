339c304982e93b9688ef2651901bfd04898deab2
SallesCosta <newcapital.in@gmail.com>
2021-10-31 21:44:55 +0100

    marcação app GitHub

--- Github/src/App.jsx
+++ Github/src/App.jsx
@@ -5,25 +5,35 @@ function App() {
     <div className='app'>
       <div className='userInfo'>
         <img src='https://avatars.githubusercontent.com/u/81476236?v=4' />
-        <h2>
+        <h1>
           <a target='_blank' href='https://github.com/SallesCosta'>Eduardo</a>
-        </h2>
+        </h1>
         <ul className='reposInfo'>
-          <li>- Repositórios: 100</li>
-          <li>- Seguidores: 10</li>
-          <li>- Seguindo: 10</li>
+          <li>Repositórios: 100</li>
+          <li>Seguidores: 10</li>
+          <li>Seguindo: 10</li>
         </ul>
 
         <div className='acitons'>
           <button>Ver repositórios</button>
           <button>Ver Favoritos </button>
         </div>
-        <ul className='repos'>
-          <li><a href='#'> Nome do Repositório</a></li>
-        </ul>
-        <ul className='starred'>
-          <li><a href='#'> Nome do Repositório</a></li>
-        </ul>
+        <div className='repos'>
+          <h2>Repositórios: </h2>
+          <ul>
+            <li>
+              <a href='#'> Nome do Repositório</a>
+            </li>
+          </ul>
+        </div>
+        <div className='starred'>
+          <h2>Favoritos: </h2>
+          <ul>
+            <li>
+              <a href='#'> Nome do Repositório</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   )
