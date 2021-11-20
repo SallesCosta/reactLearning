541e2e99787ca98a4942d2581f9b034723c58707
SallesCosta <newcapital.in@gmail.com>
2021-11-20 12:58:14 +0100

    Jest.. describe.. it/test.. expect

--- modulo2/Jest/sum.test.js
+++ modulo2/Jest/sum.test.js
@@ -1,10 +1,11 @@
 // test('testando jest', ()=>{})  tbm funciona igual
-describe('# ESCOPO', ()=>{
-it('testando jest', ()=>{})  
-it('testando jest2', ()=>{})  
-})
+
+// describe('# ESCOPO', ()=>{
+// it('testando jest', ()=>{})  
+// it('testando jest2', ()=>{})  
+//})
 
 
-it('texto se Passou', ()=> {
-    console.assert(1 === 2, 'texto se falhou')
+it('titulo do teste', ()=> {
+    expect(1).toBe(1)
 })
