3034712e0681fceea75f187e47b3156e0b470942
SallesCosta <newcapital.in@gmail.com>
2021-11-22 19:16:51 +0100

    até aqui ta tudo OK

--- modulo2/Jest/anotacoes.md
+++
@@ -1,2 +0,0 @@
-### Dependencies
-npm i --save-dev jest-cli chai babel-core babel-jest babel-preset-es2015 babel-preset-stage-0
--- modulo2/Jest/sum.test.js
+++ modulo2/Jest/sum.test.js
@@ -1,6 +1,3 @@
-const expect = require('chai')
-const sum = require('./sum')
-
-it('teste de verificação', () => {
-    expect(sum).to.is.a('function')
-}) 
+it('testando Jest', () => {
+    
+})
