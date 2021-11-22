763542e36669dffc7c18476002bb88ef9a1a310d
SallesCosta <newcapital.in@gmail.com>
2021-11-22 19:29:38 +0100

    até aqui ta tudo OK

--- modulo2/Jest/sum.test.js
+++ modulo2/Jest/sum.test.js
@@ -1,3 +1,14 @@
-it('testando Jest', () => {
-    
+
+// describe('#ESCOPO', () => {
+//     it('testando Jest', () => {})
+//     it('testando Jest2', () => {})
+// })
+
+// it('um é igual a 1', ()=> {
+//     console.assert(1===2, '1 não é igual a 2')
+// })
+
+
+it('um é igual a 1', ()=> {
+    expect(1).toBe(2)
 })
