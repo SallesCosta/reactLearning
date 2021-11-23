328bb3595cf7d379298c9141b7cc2a2a12a42a13
SallesCosta <newcapital.in@gmail.com>
2021-11-23 21:39:17 +0100

    funções recursivas

--- modulo2/Jest/map/map-recursive.js
+++ modulo2/Jest/map/map-recursive.js
@@ -1,15 +1,14 @@
 const map = (arr = [], func = (item) => item) => {
     let counter = 0
     const mapInternal = (arrInternal) => {
-        if (arr.length === 0) {
+        if (arrInternal.length === 0) {
             return []
         }
 
-        console.log(counter)
         const [head, ...tail] = arrInternal
         return [func(head, counter++)].concat(mapInternal(tail, func))
     }
-    return mapInternal(arrInternal)
+    return mapInternal(arr)
 }
 
 
