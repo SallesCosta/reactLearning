b6d7be635f757551d6ccb7270f26deb76e4ad3cc
SallesCosta <newcapital.in@gmail.com>
2021-12-05 17:06:40 +0100

    every - recursive

--- modulo2/Jest/every/every-recursive.js
+++ modulo2/Jest/every/every-recursive.js
@@ -1,9 +1,11 @@
 export const every = (arr, func) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (!func(arr[i], i, arr)) {
-      return false
-    }
-  }
+  return (function everyInternal (arrayInternal, counter) {
+    const [head, ...tail] = arrayInternal
 
-  return true
+    return arrayInternal.length === 0
+      ? true
+      : !func(head, counter, arr)
+        ? false
+        : everyInternal(tail, counter + 1)
+  })(arr, 0)
 }
