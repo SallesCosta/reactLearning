c8958e086be080fbb5bab1e44f6a511cadc9b9b9
SallesCosta <newcapital.in@gmail.com>
2021-12-05 11:16:00 +0100

    filter teste 6 - Ok

--- modulo2/Jest/filter/filter.js
+++ modulo2/Jest/filter/filter.js
@@ -1,5 +1,11 @@
  
-const filter = () => {
-    return [1, 2, 3]
+const filter = (arr, func) => {
+    let newArray = []
+    for(let i=0; i < arr.length; i++ ) {
+        if(func(arr[i], i, arr)) {
+            newArray.push(arr[i])
+        }
+    }
+    return newArray 
 }
 export default filter 
