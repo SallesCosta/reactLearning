84e27017550d72f3dc67a8f7f2c8508c96009f9d
SallesCosta <newcapital.in@gmail.com>
2021-12-05 09:53:26 +0100

    teste recursivo

--- modulo2/Jest/map/map-recursive.js
+++ modulo2/Jest/map/map-recursive.js
@@ -1,12 +1,14 @@
-const map = (arr = [], func = (item) => item) => {
-    return (function mapInternal(arrInternal, counter) {
-        const [head, ...tail] = arrInternal
-        return arrInternal.length === 0 ? [] : [
-            func(head, counter, arr),
-        ...mapInternal(tail, counter +1)
-        ]
-    })(arr, 0)
-}
 
+
+const map = (arr = [], func = (item) => item) => {
+  return (function mapInternal(arrayInternal, counter) {
+    const [head, ...tail] = arrayInternal
+
+    return arrayInternal.length === 0 ? [] : [
+      func(head, counter, arr),
+      ...mapInternal(tail, counter + 1)
+    ]
+  })(arr, 0)
+}
 
 export default map
