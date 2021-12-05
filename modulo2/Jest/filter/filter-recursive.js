a550252db31dc9f014279dcb4e32663255687f7a
SallesCosta <newcapital.in@gmail.com>
2021-12-05 11:59:45 +0100

    filter recurcivo

--- modulo2/Jest/filter/filter-recursive.js
+++ modulo2/Jest/filter/filter-recursive.js
@@ -1,11 +1,11 @@
 
 const filter = (arr, func) => {
-    return (function filterInternal(arrayInternal, counter) {
+    return (function filterInternal (arrayInternal, counter) {
         const [head, ...tail] = arrayInternal
         return arrayInternal.length === 0
             ? []
             : (func(head, counter, arr) ? [head] : [])
-                .concat(filterInternal(tail, conter + 1))
+            .concat(filterInternal(tail, counter + 1))
     })(arr, 0)
 }
 export default filter
